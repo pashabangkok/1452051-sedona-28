7b793217dd6e402bed69b53c8aec681422aa52c2
Pavel <mtvvpavel@gmail.com>
2020-07-01 22:11:50 +0300

    Js

--- js/checkin.js
+++ js/checkin.js
@@ -1,7 +1,10 @@
 var checkinButton = document.querySelector(".checkin-form-button");
-var checkinFormSection = document.querySelector(".checkin-form-section");
+var checkinFormSection = document.querySelector(".checkin-form-text");
+var checkinSection = document.querySelector(".checkin-form-section");
+
+checkinFormSection.style.display = "none";
 
 checkinButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  checkinFormSection.classList.add("ckeckin-form-closed");
+  checkinFormSection.classList.toggle("checkin-form-open");
 });
